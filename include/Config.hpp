866a8a2709c4fb0ac866fbda1e46c7b82f26ce09
mojongsab <apcbchet@gmail.com>
2026-08-16 19:08:58 +0900

    fix error return protocol

--- include/Config.hpp
+++ include/Config.hpp
@@ -4,5 +4,27 @@
 
 #ifndef KURUMI_CONFIG_HPP
 #define KURUMI_CONFIG_HPP
+#include <filesystem>
+#include <cinttypes>
 
+namespace kurumi {
+
+    struct Config {
+
+        struct Discord {
+            std::string bot_token;
+            std::string guild_id;
+            std::string channel_id;
+        };
+
+        Config() = default;
+
+        std::string toss_api_key;
+        std::string naver_api_key;
+        Discord discord;
+
+        static std::optional<Config> load(std::filesystem::path config_path);
+        static bool save(std::filesystem::path config_path, const Config& config);
+    };
+}
 #endif //KURUMI_CONFIG_HPP
